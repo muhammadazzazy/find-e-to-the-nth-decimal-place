f7ce74311fcac5de234effc2236bd2fd2708b349
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-02 11:18:20 +0200

    refactored the code a bit

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from math import e
 from sys import exit
 
-LIMIT: int = 15
-print(f'Welcome to The ℇ Approximator!')
+def main() -> None:
+    LIMIT: int = 15
+    print(f'Welcome to The ℇ Approximator!')
 
-EULER: str = str(e)
+    EULER: str = str(e)
 
-prompt: str = f'Enter the number of decimal places (up to {LIMIT}): '
+    prompt: str = f'Enter the number of decimal places (up to {LIMIT}): '
 
-while True:
-    user_input: str = input(prompt)
+    while True:
+        user_input: str = input(prompt)
 
-    if user_input == 'exit':
-        print('Thanks for trying my program!')
-        exit()
+        if user_input == 'exit':
+            print('Thanks for trying my program!')
+            exit()
 
-    if not user_input.isnumeric():
-        print('Please enter a whole number between 0 and 15...')
-        continue
+        if not user_input.isnumeric():
+            print('Please enter a whole number between 0 and 15...')
+            continue
     
-    n: int = int(user_input)
-    print(f"ℇ to the {n}th decimal place is {EULER[:n+2]}")
+        n: int = int(user_input)
+        print(f'ℇ to the {n}th decimal place is {EULER[:n+2]}')
+
+main()
